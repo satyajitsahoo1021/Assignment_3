130fd0bbe6595c4a2b648c7c1df116df606d0dad
satyajitsahoo1021 <satyajitsahoo1021@gmail.com>
2026-02-17 00:24:00 +0530

    Added two files

---
+++ Task2.py
@@ -0,0 +1,5 @@
+import math
+number = int(input("Enter a number : "))
+print(f"The square root of {number} is {math.sqrt(number)}")
+print(f"Natural logarithm of {number} is {math.log(number)}")
+print(f"The sine of {number} is {math.sin(number)}")
